757bc512f4da14431ace123f0ec27905778d6fb5
Felipe <felipedelucanogueira@gmail.com>
2021-05-01 19:00:09 -0300

    Start Project

---
+++ lib/movie_view.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+
+class MovieView extends StatefulWidget {
+  @override
+  _MovieViewState createState() => _MovieViewState();
+}
+
+class _MovieViewState extends State<MovieView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
